55cac69e181f05e808a1c7070522617ea8acc628
Muneeb Asmone <92797864+milenek-x@users.noreply.github.com>
2023-04-03 05:01:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from instaloader import Instaloader, Profile
 
 L = instaloader.Instaloader()
 
-PROFILE = "YOUR_INSTAGRAM_USERNAME"
+PROFILE = input("Enter the instagram profile name: ")
 profile = Profile.from_username(L.context, PROFILE)
 
 posts_sorted_by_likes = sorted(profile.get_posts(), key=lambda post: post.likes, reverse=True)
